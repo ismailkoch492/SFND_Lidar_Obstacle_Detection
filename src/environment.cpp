430fa01cd643df8ebac828e2970c5dfa3eaaa379
İsmail Koç <ismailkoch492@gmail.com>
2022-11-28 21:12:06 +0300

    Streaming main PCD

--- src/environment.cpp
+++ src/environment.cpp
@@ -256,7 +256,7 @@ int main (int argc, char** argv)
         case realCloud:
         {
             ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-            std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2");
+            std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
             auto streamIterator = stream.begin();
             pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
             //cityBlock(viewer, pointProcessorI, inputCloudI); //CustomRANSAC: Floating point exception (core dumped) due to empty point cloud
